89e2b7fa77407c03bcae4dc4bf14cfcc6fb26c6c
ehoda8 <addblogs12@gmail.com>
2022-08-17 20:04:15 +0200

    update circleci

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "udagram-api-dev",
-  apiHost: "http://udagram-api-dev.eba-df8iuxhr.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'udagram-api-dev',
+  apiHost: 'http://udagram-api-dev.eba-df8iuxhr.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: "udagram-api-dev",
-  apiHost: "http://udagram-api-dev.eba-df8iuxhr.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'udagram-api-dev',
+  apiHost:
+    'http://udagram-api-dev.eba-df8iuxhr.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
