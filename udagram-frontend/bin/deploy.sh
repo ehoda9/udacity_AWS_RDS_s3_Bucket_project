6a65a35a913cddd7f58433c6bf84362e20ef7bc8
ehoda8 <addblogs12@gmail.com>
2022-08-18 07:25:50 +0200

    fix issue and updates

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,19 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagrambucket/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagrambucket/
-# Configure Environment Variables
-eb list 
-eb use udagram-api-dev
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv PORT=$PORT
-eb setenv DBPORT=$DBPORT
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv AWS_REGION=$AWS_REGION
-eb setenv AWS_PROFILE=$AWS_PROFILE
-eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv URL=$URL
-eb health
